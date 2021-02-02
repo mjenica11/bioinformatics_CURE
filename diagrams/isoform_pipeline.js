144db4e49c3feaaf9a8cd7de3b859208aefcf49e
Mollie Peters <mjpete11@asu.edu>
2021-02-02 12:58:31 -0700

    diagram of isoform pipeline

---
+++ diagrams/isoform_pipeline.js
@@ -0,0 +1,10 @@
+graph TD
+  A[RNA seq data] --> B[fastqc_raw]
+  B[fastqc_raw] --> C[multiqc_raw]
+  A[RNA seq data] --> D[trimming]
+  D[trimming] --> |test parameters|E[fastqc_trimmed]
+  E[fastqc_trimmed] --> F[multiqc_trimmed]
+  D[trimming] --> |test parameters|G[quantification]
+  G[quantification] --> H[prepare_count_matrix]
+  H[prepare_count_matrix] --> J[differential_expression]
+  J[differential_expression] --> K[plot_results]
