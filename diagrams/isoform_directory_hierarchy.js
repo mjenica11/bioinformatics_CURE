beffcb6d3f120d473c278e5dc051f8e5a0f481e1
Mollie Peters <mjpete11@asu.edu>
2021-02-02 13:16:29 -0700

    added files, turned sideways

--- diagrams/isoform_directory_hierarchy.js
+++ diagrams/isoform_directory_hierarchy.js
@@ -1,9 +1,11 @@
-graph TD
+graph LR
   A[snakefile] --> B[scripts]
   A[snakefile] --> C[data]
   A[snakefile] --> D[quality_control]
   A[snakefile] --> E[quantification]
   A[snakefile] --> F[differential_expression]
+  C[data] --> adapter_sequences.fa
+  C[data] --> h{{samples.config}}
   C[data] --> G[raw]
   C[data] --> H[trimmed]
   D[quality_control] --> I[raw]
@@ -12,13 +14,18 @@ graph TD
   E[quantification] --> L[samples]
   L[samples] --> Q{{sample quant.sf files}}
   L[samples] --> R{{count_matrix.csv}}
-  F[differential_expression] --> N{{DE_genes}}
-  F[differential_expression] --> O{{Volcano plot}}
-  F[differential_expression] --> P{{MD plot}}
+  F[differential_expression] --> N{{results.csv}}
+  F[differential_expression] --> O{{volcano_plot.pdf}}
+  F[differential_expression] --> P{{md_plot.pdf}}
   B[scripts] --> T{{combine_counts.R}}
   B[scripts] --> U{{differential_expression.R}}
-  B[scripts] --> V{{plots.R}}
+  B[scripts] --> V{{volcano_plot.R}}
+  B[scripts] --> b{{md_plot.R}}
   G[raw] --> W{{raw fastas}}
   H[trimmed] --> X{{trimmed fastas}}
-  J[trimmed] --> Z{{sample and aggregate html reports}}
-  I[raw] --> a{{sample and aggregate html reports}}
+  J[trimmed] --> c[sample]
+  c[sample] --> Z{{individual html reports}}
+  I[raw] --> d[sample]
+  d[sample] --> e{{individual html reports}}
+  I[raw] --> f{{multiqc report}}
+  J[trimmed] --> g{{multiqc report}}
