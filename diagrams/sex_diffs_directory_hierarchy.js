da6d5f8953355edd089ddd87bbb73c2bc7501341
Mollie Peters <mjpete11@asu.edu>
2021-02-05 13:48:45 -0700

    added sex specific reference index directories

--- diagrams/sex_diffs_directory_hierarchy.js
+++ diagrams/sex_diffs_directory_hierarchy.js
@@ -10,12 +10,15 @@ graph LR
   D[quality_control] --> I[raw]
   D[quality_control] --> J[trimmed]
   E[quantification] --> K[index]
+  K[index] --> banana[XY_reference]
+  K[index] --> apple[XX_reference]
   E[quantification] --> L[samples]
   I[raw] --> d[sample]
   C[data] --> adapter_sequences.fa
   C[data] --> h{{samples.config}}
   L[samples] --> Q{{sample quant.sf files}}
-  E[quantification] --> R{{count_matrix.csv}}
+  E[quantification] --> R{{female_count_matrix.csv}}
+  E[quantification] --> AB{{male_count_matrix.csv}}
   F[differential_expression] --> N{{results.csv}}
   F[differential_expression] --> O{{volcano_plot.pdf}}
   F[differential_expression] --> P{{md_plot.pdf}}
@@ -24,7 +27,6 @@ graph LR
   B[scripts] --> U{{differential_expression.R}}
   B[scripts] --> V{{volcano_plot.R}}
   B[scripts] --> b{{md_plot.R}}
-  B[scripts] --> k{{venn_diagram.R}}
   G[raw] --> W{{raw fastas}}
   H[trimmed] --> X{{trimmed fastas}}
   c[sample] --> Z{{individual html reports}}
